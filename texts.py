f3519e76ddfe41063527209558128af8d8820ecf
levelakc <97440174+levelakc@users.noreply.github.com>
2024-10-21 18:41:18 +0300

    Create texts.py

---
+++ texts.py
@@ -0,0 +1,23 @@
+starttxt = 
+main_pic = 
+ORDERtxt = "🛬להזמנה - לחץ כאן🛫"
+GROUPtxt = "לקבוצה👨‍👧‍👦"
+ADMINtxt = ""
+ADMINurl = ""
+NEXTbtntxt = "➡️הבא"
+BACKbtntxt = "הקודם⬅️"
+SPACEbtntxt= " "
+BACKmbtntxt = "חזרה"
+BACKmmbtntxt = "חזרה לתפריט הראשי"
+
+RecipeGID = 
+RecipeGIDorg = 
+LogsGID = 
+aID = 
+privateGlink = 
+
+posttxt = 
+
+# Define the TRAFFICGID and admin URL
+TRAFFICGID = 
+adminURL = 
